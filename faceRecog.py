74df9e0ff755bb5300d424bf7c983c18753cf1c9
dawierha <daniel.e_95@hotmail.com>
2020-05-10 21:59:38 +0100

    opens piCamera with with

--- faceRecog.py
+++ faceRecog.py
@@ -69,60 +69,62 @@ def motorThread(in_q, en_g):
 
 def camThread(out_q, en_q):
     #Sets up the camera
-    camera = PiCamera()
-    camera.resolution = (640, 480)
-    camera.framerate = 5
-    camera.exposure_mode = 'night'
-    #Sets digital gain to 8.0
-    mmal.mmal_port_parameter_set_rational(camera._camera.control._port, mmal.MMAL_PARAMETER_GROUP_CAMERA + 0x5A, to_rational(8.0))
-    rawCapture = PiRGBArray(camera, size=(640, 480))
-    
-
-    width = camera.resolution[0]
-    time_stamp_old = time.perf_counter() 
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        #print(f"target fps: {camera.framerate}, dg: {camera.digital_gain}, exposure: {camera.exposure_mode}")
-        # Read the frame
-        img = frame.array
-
-        # Convert to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    with PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.framerate = 5
+        camera.exposure_mode = 'night'
+        #Sets digital gain to 8.0
+        mmal.mmal_port_parameter_set_rational(camera._camera.control._port, mmal.MMAL_PARAMETER_GROUP_CAMERA + 0x5A, to_rational(8.0))
+        rawCapture = PiRGBArray(camera, size=(640, 480))
         
-        # Detect the faces
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-        # Draw the rectangle around each face
-        enable = False
-        for (x, y, w, h) in faces:
-            enable = True
+
+        width = camera.resolution[0]
+        time_stamp_old = time.perf_counter() 
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            #print(f"target fps: {camera.framerate}, dg: {camera.digital_gain}, exposure: {camera.exposure_mode}")
+            # Read the frame
+            img = frame.array
+
+            # Convert to grayscale
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            
+            # Detect the faces
+            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+            
+            # Draw the rectangle around each face and detects decides if it wants to go left or right
+            enable = False
+            for (x, y, w, h) in faces:
+                enable = True
+                if args.show:
+                    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                center = (x+w/2, y+h/2)
+                if center[0] <= width/2:
+                    #print("Left half of image")
+                    out_q.put(GPIO.HIGH)
+                else:
+                    #print("Right half of image")
+                    out_q.put(GPIO.LOW)
+            en_q.put(enable)
+            
+            # Display
             if args.show:
-                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            center = (x+w/2, y+h/2)
-            if center[0] <= width/2:
-                #print("Left half of image")
-                out_q.put(GPIO.HIGH)
-            else:
-                #print("Right half of image")
-                out_q.put(GPIO.LOW)
-        en_q.put(enable)
-        
-        # Display
-        if args.show:
-            cv2.imshow('img', img)
-        # Stop if escape key is pressed
-        k = cv2.waitKey(30) & 0xff
-        if k==27:
-            break
-        
-        # clear the stream in preparation for the next frame    
-        rawCapture.truncate(0)
-        
-        #Calculates and prints the FPS to std out
-        if args.fps:
-            time_stamp_new = time.perf_counter() 
-            fps = 1/(time_stamp_new-time_stamp_old)
-            sys.stdout.write("\rFPS: {0}\t".format(round(fps, 1)))
-            sys.stdout.flush()
-            time_stamp_old = time_stamp_new
+                cv2.imshow('img', img)
+            # Stop if escape key is pressed
+            k = cv2.waitKey(30) & 0xff
+            if k==27:
+                break
+            
+            # clear the stream in preparation for the next frame    
+            rawCapture.truncate(0)
+            rawCapture.seek(0)
+            
+            #Calculates and prints the FPS to std out
+            if args.fps:
+                time_stamp_new = time.perf_counter() 
+                fps = 1/(time_stamp_new-time_stamp_old)
+                sys.stdout.write("\rFPS: {0}\t".format(round(fps, 1)))
+                sys.stdout.flush()
+                time_stamp_old = time_stamp_new
 
 #Arguemnts parsing
 parser = argparse.ArgumentParser(description="Mirror Pi - The mirror that\'s avoiding you")
